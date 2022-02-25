f69503a1deb53295332c6f2459b570747e5878fe
achmadberudu <amadmandala@gmail.com>
2022-02-25 14:10:02 +0700

    coba git

---
+++ BasicGit/BasicGit/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BasicGit
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("From Herul Syahwandi");
+            Console.WriteLine("From Herul Syahwandi versi 2");
+            Console.WriteLine(" Reo Sahobby");
+        }
+
+        static void Herul()
+        {
+            Console.WriteLine("From Herul Syahwandi versi 2");
+        }
+
+        static void Achmad()
+        {
+            Console.WriteLine("From Achmad Nur Fauzi");
+        }
+
+        static void Denny()
+        {
+            Console.WriteLine("Hai from Denny");
+        }
+
+        static void Rizky()
+        {
+            Console.WriteLine("Kode ini dikerjakan oleh Rizky");
+        }
+        
+        static void Reo()
+        {
+            Console.WriteLine("ini dikerjakan oleh Reo");
+
+        }
+
+        static void Rezky()
+        {
+            Console.WriteLine("Test dari bracng REZKY");
+        }
+
+        static void Hanif()
+        {
+            Console.WriteLine("Fahri Hanif");
+        }
+    }
+}
